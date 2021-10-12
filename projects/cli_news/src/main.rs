e498f800804afb8249d84bae51bf4417e56ad357
Daniel Rubio <drubio432@gmail.com>
2021-10-11 22:11:35 -0400

    parse data from api

--- projects/cli_news/src/main.rs
+++ projects/cli_news/src/main.rs
@@ -1,3 +1,26 @@
+use serde::Deserialize;
+use std::error::Error;
+
+const API_URL:&str = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=7c13465f9524488ca298a43871b1d4c7";
+
+#[derive(Deserialize, Debug)]
+struct Articles {
+    articles: Vec<Article>,
+}
+#[derive(Deserialize, Debug)]
+struct Article {
+    title: String,
+    url: String,
+}
+
+fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
+    let response = ureq::get(API_URL).call()?.into_string()?;
+    let articles: Articles = serde_json::from_str(&response)?;
+
+    Ok(articles)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let articles = get_articles(API_URL);
+    dbg!(articles);
 }
